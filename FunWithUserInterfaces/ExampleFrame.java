b0790db28ae1584b24a21944935bccc6c2fcad21
Rudra Patel <rudrap26176@gmail.com>
2023-11-29 11:47:40 -0600

    GUI Example

--- FunWithUserInterfaces/ExampleFrame.java
+++ FunWithUserInterfaces/ExampleFrame.java
@@ -11,12 +11,51 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.BorderLayout;
 import javax.swing.JOptionPane;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 /**
  * This is how to create a simple userinterface with buttons, labels etc.
  */
 
 public class ExampleFrame extends JFrame implements ActionListener {
+
+  private JTextField txtText;
+  private JButton buttonClose;
+
+  public void setUpMenu() {
+    JMenuBar bar = new JMenuBar();
+    setJMenuBar(bar);
+
+    JMenu menuFile = new JMenu("File");
+    bar.add(menuFile);
+
+    JMenuItem miSayHi = new JMenuItem("Say Hi");
+    menuFile.add(miSayHi);
+
+    JMenuItem miExit = new JMenuItem("Exit");
+    menuFile.add(miExit);
+
+    JMenu menuHelp = new JMenu("Help");
+    bar.add(menuHelp);
+
+    JMenuItem miAbout = new JMenuItem("About");
+    menuHelp.add(miAbout);
+
+    miSayHi.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(null, "Hi!");
+      }
+    });
+
+    miExit.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        System.exit(0);
+      }
+    });
+
+  }
 
   public void setUpGUI() {
     setTitle("Example User Interface");
@@ -35,19 +74,31 @@ public class ExampleFrame extends JFrame implements ActionListener {
 
     c.add(panSouth, BorderLayout.SOUTH);
 
+    buttonClose = new JButton("Close");
+    buttonClose.addActionListener(this);
+    panSouth.add(buttonClose);
+
     JPanel panCenter = new JPanel();
     panCenter.setLayout(new FlowLayout());
     JLabel lblText = new JLabel("Enter text: ");
-    JTextField txtText = new JTextField(20);
+    txtText = new JTextField(20);
+    txtText.addActionListener(this);
     panCenter.add(lblText);
     panCenter.add(txtText);
     c.add(panCenter, BorderLayout.CENTER);
 
+    setUpMenu();
   }
 
   @Override
   public void actionPerformed(ActionEvent e) {
-    JOptionPane.showMessageDialog(null, "You clicked the button!");
+    if (e.getSource() == txtText) {
+      JOptionPane.showMessageDialog(null, "You clicked enter in the textfield");
+    } else if (e.getSource() == buttonClose) {
+      System.exit(0);
+    } else {
+      JOptionPane.showMessageDialog(null, txtText.getText());
+    }
   }
 
   public ExampleFrame() {
